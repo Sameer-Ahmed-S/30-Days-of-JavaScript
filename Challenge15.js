625c659fe851d5f6b2d2463f9c428400c6990bb0
Sameer Ahmed S <sameer.contrib@gmail.com>
2023-11-20 21:10:01 +0530

    added chanllenge 15
    Signed-off-by: Sameer Ahmed S <sameer.contrib@gmail.com>

---
+++ Challenge15.js
@@ -0,0 +1,28 @@
+/*
+
+**Challenge 15**
+Problem Description -
+create a string_chop function that takes a string and chop factor(size) and chops out the string into the factor(size) that has been passed
+Test Data :
+console.log(string_chop('Scaler'));
+console.log(string_chop('Scaler',2));
+["Scaler"]
+['Sc' , 'al' ,'er']
+
+
+*/
+
+function string_chop(str, size) {
+    let arr = [];
+    if(size === undefined || size === 0)
+        size = str.length
+    for (let i = 0; i < str.length; i += size) {
+        arr.push(str.slice(i, i + size));
+    }
+    return arr;
+}
+
+
+console.log(string_chop('Scaler'));
+console.log(string_chop('Scaler', 2));
+console.log(string_chop('Scaler', 0));
