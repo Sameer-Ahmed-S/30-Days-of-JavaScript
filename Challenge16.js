b1c3405f64f5c5d32d171396ec5b529f8118fd11
Sameer Ahmed S <sameer.contrib@gmail.com>
2023-11-23 08:52:27 +0530

    Added Challenge16
    Signed-off-by: Sameer Ahmed S <sameer.contrib@gmail.com>

---
+++ Challenge16.js
@@ -0,0 +1,23 @@
+/*
+**Challenge 16**
+Problem Description:
+Write a JavaScript function named sumArray that takes an array of numbers and returns the sum of all the numbers in the array using the reduce() method.
+Inputs:
+An array of numbers.
+Outputs:
+A number representing the sum of the array.
+Hints:
+The reduce() method applies a function against an accumulator and each element in the array (from left to right) to reduce it to a single value.
+Solution Approach:
+Initialize the reduce() method with an initial accumulator value of 0. Then, for each value in the array, add it to the accumulator.
+*/
+
+function sumArray(arr) {
+ 
+    return arr.reduce((acc,cur)=> acc+cur,0 )   
+}
+
+
+console.log(sumArray([1, 2, 3, 4, 5, 6])); // 21
+
+console.log(sumArray([])); // 0  
